73afcea1706552caa2ba2f5279cd5e67683f07a9
TANAKADAMON\kizan <kizan246@gmail.com>
2025-04-22 12:23:36 +0900

    localstrage

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 https://memo-jet-eta.vercel.app/
 
 ## 機能
-・新規登録
-・編集
+・新規登録  
+・編集  
 ・削除
--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,9 @@
 import { RouterLink, RouterView } from 'vue-router'
 import inputMemo from './components/inputMemo.vue';
 import memoList from './components/memoList.vue';
-import { ref } from 'vue';
+import { ref, watch, onMounted } from 'vue';
 
-const memos = ref([
-]);
+const memos = ref([]);
 
 function addMemo(newMemo){
   memos.value.push(newMemo);
@@ -13,7 +12,22 @@ function addMemo(newMemo){
 function deleteMemo(index){
   memos.value.splice(index, 1)
 }
+const STORAGE_KEY = 'my-memos'
 
+onMounted(() => {
+  const saved = localStorage.getItem(STORAGE_KEY)
+  if (saved) {
+    try {
+      memos.value = JSON.parse(saved)
+    } catch (e) {
+      console.error('JSONの読み込みエラー:', e)
+    }
+  }
+})
+
+watch(memos, (newVal) => {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(newVal))
+}, { deep: true })
 </script>
 
 <template>
